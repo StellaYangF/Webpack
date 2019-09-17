0def6dbc49941563c27c840d071d334f0388428a
StellaYangFan <1062878468@qq.com>
2019-09-17 17:29:08 +0800

    update webpack.prod.js

--- webpack-1-practice/build/webpack.prod.js
+++ webpack-1-practice/build/webpack.prod.js
@@ -1,7 +1,15 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
   ]
